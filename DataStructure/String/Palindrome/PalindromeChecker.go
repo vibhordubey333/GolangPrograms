f641b3c54d8033890f302f94f91b0fbe3327db41
Vibhor Dubey <vibhordubey333@gmail.com>
2021-06-20 09:28:46 +0530

    DataStrucutre|String|PalindromeChecker

--- DataStructure/String/Palindrome/PalindromeChecker.go
+++ DataStructure/String/Palindrome/PalindromeChecker.go
@@ -2,20 +2,51 @@
 // Click here and start typing.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+	)
+
 
 func main() {
 
 	userString := "RADAR"
-	PalindromeChecker(userString)
+	palindromeChecker(userString)
+	optimizedpalindromeChecker(userString)
 	userString = "MALAYALAM"
-	PalindromeChecker(userString)
+	palindromeChecker(userString)
+	optimizedpalindromeChecker(userString)
 	userString = "XMALAYALAM"
-	PalindromeChecker(userString)
+	palindromeChecker(userString)
+	optimizedpalindromeChecker(userString)
 
 }
+func optimizedpalindromeChecker(userString string) {
 
-func PalindromeChecker(userString string) {
+	start := time.Now()
+
+	j := len(userString) - 1
+	flag := 0
+	stringLength := len(userString)/2
+	for i := 0; i < stringLength; i++ {
+		if userString[i] == userString[j] {
+			flag++
+		}
+		j--
+	}
+
+	if flag == stringLength {
+		fmt.Println("Palindrome")
+	} else {
+		fmt.Println(" Not Palindrome")
+	}
+	elapsed := time.Since(start)
+	fmt.Println("optimizedpalindromeChecker",elapsed)
+}
+
+
+func palindromeChecker(userString string) {
+	start := time.Now()
 	sliceArray := []rune(userString) // Cannot convert into []string array
 
 	flag := 0
@@ -31,4 +62,6 @@ func PalindromeChecker(userString string) {
 	} else {
 		fmt.Println("Not Palindrome")
 	}
+	elapsed := time.Since(start)
+	fmt.Println("palindromeChecker: ",elapsed)
 }
