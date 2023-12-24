080adb094048baaf4f89161a8b5badfe2c7be664
Vibhor Dubey <vibhordubey333@gmail.com>
2023-12-24 12:42:51 +0530

    GolangPrograms|Concurrent server

---
+++ GoPlayground/Go-Server-Handling-Concurrent-Requests/Readme.md
@@ -0,0 +1,7 @@
+#### To Perform Load Testing.
+
+1. Install Apache AB.
+2. Fire up command `ab -c 500 -n 500 http://localhost:7777 ` <br/>
+    `-c` : Concurrency
+    `-n` : No. of requests
+    
--- GoPlayground/Go-Server-Handling-Concurrent-Requests/server.go
+++ GoPlayground/Go-Server-Handling-Concurrent-Requests/server.go
@@ -23,3 +23,58 @@ func main() {
 		panic(err)
 	}
 }
+
+/*
+========Apache AB==========
+Output:
+ab -c 500 -n 500 http://localhost:7777/counter
+This is ApacheBench, Version 2.3 <$Revision: 1903618 $>
+Copyright 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/
+Licensed to The Apache Software Foundation, http://www.apache.org/
+
+Benchmarking localhost (be patient)
+Completed 100 requests
+Completed 200 requests
+Completed 300 requests
+Completed 400 requests
+Completed 500 requests
+Finished 500 requests
+
+
+Server Software:
+Server Hostname:        localhost
+Server Port:            7777
+
+Document Path:          /counter
+Document Length:        0 bytes
+
+Concurrency Level:      500
+Time taken for tests:   1.220 seconds
+Complete requests:      500
+Failed requests:        0
+Total transferred:      37500 bytes
+HTML transferred:       0 bytes
+Requests per second:    409.71 [#/sec] (mean)
+Time per request:       1220.388 [ms] (mean)
+Time per request:       2.441 [ms] (mean, across all concurrent requests)
+Transfer rate:          30.01 [Kbytes/sec] received
+
+Connection Times (ms)
+              min  mean[+/-sd] median   max
+Connect:        0    0   0.4      0       4
+Processing:   500  513  10.9    513     710
+Waiting:      500  512   6.5    512     528
+Total:        501  513  10.9    513     710
+
+Percentage of the requests served within a certain time (ms)
+  50%    513
+  66%    516
+  75%    518
+  80%    519
+  90%    522
+  95%    524
+  98%    525
+  99%    527
+ 100%    710 (longest request)
+
+*/
